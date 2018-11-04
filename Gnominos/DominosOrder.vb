821067b351db1998846ad56bfa910ac24d083d5e
Luke <twogbsd@gmail.com>
2018-11-04 12:48:54 -0800

    Minor tweaks.
    - Added Add method to DominosOrder
    - Added Remove method to DominosOrder
    - Tweaked some XML docs.
    
    Very minor tweaks. Still no logic in any of these.

--- Gnominos/DominosOrder.vb
+++ Gnominos/DominosOrder.vb
@@ -26,10 +26,16 @@ Public NotInheritable Class DominosOrder
     Public ReadOnly Property Menu As DominosMenu
 
     ''' <summary>
-    ''' The service method of this <see cref="DominosOrder"/>.
+    ''' How this <see cref="DominosOrder"/> will be fulfilled.
     ''' </summary>
     ''' <returns>A <see cref="ServiceMethod"/> enum.</returns>
     Public ReadOnly Property Type As ServiceMethod
+
+    ''' <summary>
+    ''' A list of the items to be ordered.
+    ''' </summary>
+    ''' <returns>An <see cref="IReadOnlyList(Of DominosItem)"/></returns>
+    Public ReadOnly Property Items As IReadOnlyList(Of DominosItem)
 
     ''' <summary>
     ''' Creates a new instance of a <see cref="DominosOrder"/>. 
@@ -41,10 +47,28 @@ Public NotInheritable Class DominosOrder
 
     ''' <summary>
     ''' Creates a new instance of a <see cref="DominosOrder"/>. 
-    ''' <para>The <see cref="DominosStore"/> that is passed in will be the store that will fulfill this new <see cref="DominosOrder"/>.</para>
+    ''' <para>The <see cref="DominosStore"/> that is passed in will be the store to fulfill this new <see cref="DominosOrder"/>.</para>
     ''' </summary>
     Sub New(customer As DominosCustomer, store As DominosStore, orderType As ServiceMethod)
         Throw New NotImplementedException
     End Sub
+
+    ''' <summary>
+    ''' Add an item to this <see cref="DominosOrder"/>.
+    ''' </summary>
+    ''' <param name="item">Any <see cref="DominosItem"/> from the menu or a custom pizza from the <see cref="DominosPizzaBuilder"/></param>
+    ''' <returns>A <see cref="Boolean"/> indicating if this action was successful.</returns>
+    Public Function Add(item As DominosItem) As Boolean
+        Throw New NotImplementedException
+    End Function
+
+    ''' <summary>
+    ''' Remove an item from this <see cref="DominosOrder"/>.
+    ''' </summary>
+    ''' <param name="item">Any <see cref="DominosItem"/> from <see cref="Items"/>.</param>
+    ''' <returns>A <see cref="Boolean"/> indicating if this action was successful.</returns>
+    Public Function Remove(item As DominosItem) As Boolean
+        Throw New NotImplementedException
+    End Function
 
 End Class
